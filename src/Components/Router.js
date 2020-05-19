a512dc7b3af17447ec18304140261ae05f4855b0
Sukjun Sagong <ricepotato40@gmail.com>
2020-05-19 20:19:06 +0900

    update routes

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "./Home";
-import Mf from "../Routes/Mf";
-
+import Mf from "../Views/Mf";
 export default () => (
   <Router>
     <>
